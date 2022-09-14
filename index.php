2241e7289784906fb12f8804cce4c90f42ff08e0
Aarrez <aaronmhj01@outlook.com>
2022-09-14 10:42:17 +0200

    javaScript, Start of portfolio site

--- index.php
+++ index.php
@@ -4,12 +4,39 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="bootstrap-5.2.1-dist\css\bootstrap.min.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="bootstrap-5.2.1-dist\js\bootstrap.min.js" crossorigin="anonymous">
     <title>My Website</title>
   </head>
   <body>
-    <div class="Header">
-      <img src="FishInPond.jpg" alt="">
-    </div>
+    <nav class="navbar navbar-expand-lg fixed-top navbarScroll">
+        <div class="container">
+            <a class="navbar-brand" href="#">Brad</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#home">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#about">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#services">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#portfolio">Portfolio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#contact">Contact</a>
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+    </nav>
     <!--
     <h1>Hello please put in you valuable private info</h1>
 
@@ -24,3 +51,5 @@
 
   </body>
 </html>
+
+<!-- Latest compiled and minified CSS -->
